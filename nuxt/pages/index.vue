08094ce3dad29636e2b93fbffcd55ac5cd47e191
Christian Gil <christian@chrisgvdev.com>
2022-06-24 17:11:37 -0500

    remove(Nuxt): Remove nuxt package

--- nuxt/pages/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { Switch } from '@headlessui/vue';
-
-const enabled = ref(true);
-</script>
-
-<template>
-  <div class="text-5xl font-bold text-gray-900">Hello</div>
-</template>
